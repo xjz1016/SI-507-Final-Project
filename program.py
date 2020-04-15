ad7c2f7ef64c7f05fd9d9d763805d95b1ed9bd62
Junzhe Xu <junzhexu@umich.edu>
2020-04-15 12:32:32 -0400

    Added docstring

--- program.py
+++ program.py
@@ -24,6 +24,26 @@ app = Flask(__name__)
 #########################################
 
 class City:
+    '''deifinition of the class city
+
+    Instance Attributes
+    -------------------
+    id_pos: int
+        the id(position) of the city instance in the data
+    name: string
+        the name of the city instance
+    state: string
+        the state name that the city is located at
+    population: int
+        the population of the city
+    area: float
+        the area of the city in Square mile
+    latitude: string
+        the latitude of the city center
+    longitude: string
+        the longitude of the city center
+    
+    '''
     def __init__(self, id_pos=0, name=None, state=None, population=0, area=0, latitude='', longitude=''):
         self.id_pos = id_pos
         self.name = name
@@ -34,6 +54,31 @@ class City:
         self.longitude = longitude
 
 class Restaurant:
+    '''definition of the class restaurant
+
+     Instance Attributes
+    -------------------
+    rating: int
+        the rating for the restaurant in the range [0, 5]
+    price: int
+        the rating for the restaurant in the range [1, 3]
+    phone: string
+        the phone number for the restaurant
+    category: string
+        the category that the restaurant belong to
+    yelp_id: string
+        the unique identifer for the restaurant
+    url: string
+        the website for the restaurant on Yelp
+    review_num: int
+        the number of reviews of the restaurant
+    name: string
+        the official name of the restaurant
+    city: string
+        the city name that the restaurant located at
+    state: string
+        the state name that the restaurant is located at
+    '''
     def __init__(self, rating=0, price=None, phone='', category='', yelp_id='', url='', 
                  review_num=0, name='', city='', state=''):
         self.rating = rating
@@ -97,6 +142,16 @@ CACHE_DICT = load_cache(CACHE_FILE)
 #########################################
 
 def db_create_table_cities():
+    ''' create the table named "Cities" in the database
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    none
+    '''
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
     # drop_cities_sql = 'DROP TABLE IF EXISTS "Cities"'
@@ -117,6 +172,16 @@ def db_create_table_cities():
     conn.close()
 
 def db_create_table_restaurants():
+    ''' create the table named "Restaurants" in the database
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    none
+    '''
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
     # drop_restaurants_sql = 'DROP TABLE IF EXISTS "Restaurants"'
@@ -141,6 +206,17 @@ def db_create_table_restaurants():
     conn.close()
 
 def db_write_table_cities(city_instances):
+    ''' write data into the table "Cities" in the database
+    
+    Parameters
+    ----------
+    city_instances: list
+        a list of city instances
+    
+    Returns
+    -------
+    none
+    '''
     insert_cities_sql = '''
         INSERT OR IGNORE INTO Cities
         VALUES (?, ?, ?, ?, ?, ?, ?)
@@ -155,6 +231,17 @@ def db_write_table_cities(city_instances):
     conn.close()
 
 def db_write_table_restaurants(restaurant_instances):
+    ''' write data into the table "Restaurants" in the database
+    
+    Parameters
+    ----------
+    restaurant_instances: list
+        a list of restaurant instances
+    
+    Returns
+    -------
+    none
+    '''
     insert_restaurants_sql = '''
         INSERT OR IGNORE INTO Restaurants
         VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
@@ -170,6 +257,17 @@ def db_write_table_restaurants(restaurant_instances):
     conn.close()
 
 def build_city_instance():
+    ''' function that scrapes the wikpedia page and build city instances 
+    
+    Parameters
+    ----------
+    none
+    
+    Returns
+    -------
+    city_instances: list
+        a list of 314 different city instances
+    '''
     # CACHE_DICT = load_cache(CACHE_FILE)
     city_instances = []
     site_url = 'https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population'
@@ -197,6 +295,19 @@ def build_city_instance():
     return city_instances
 
 def build_restaurant_instance(city_instances):
+    ''' function that searches restaurant in different cities
+        according to the city_instances list from the Yelp Fusion API 
+    
+    Parameters
+    ----------
+    city_instances: list
+        a list of 314 different city instances
+    
+    Returns
+    -------
+    restaurant_instances: list
+        a list of thousands of different restaurant instances
+    '''
     # CACHE_DICT = load_cache(CACHE_FILE)
     restaurant_instances = []
     endpoint_url = 'https://api.yelp.com/v3/businesses/search'
@@ -287,6 +398,17 @@ def make_url_request_using_cache(url_or_uniqkey, params=None):
     return CACHE_DICT[url_or_uniqkey]
 
 def build_database():
+    '''initialize and create the database according to the city instances 
+       and restaurant instances (either fetch or use cache)
+    
+    Parameters
+    ----------
+    none
+    
+    Returns
+    -------
+    none
+    '''
     print('Building database...')
     city_instances = build_city_instance()
     db_create_table_cities()
@@ -320,6 +442,19 @@ def searchDB(query):
 #########################################
 
 def process_name(city_name):
+    '''process the input name in order to make it compatible with the database
+    (e.g., change the name "new york" into "New York")
+    
+    Parameters
+    ----------
+    city_name: string
+        the name of the city before formatting
+    
+    Returns
+    -------
+    city_name: string
+        the name of the city after formatting
+    '''
     split = city_name.split(' ')
     res = ''
     for word in split:
@@ -330,6 +465,21 @@ def process_name(city_name):
     return res.strip()
 
 def get_avg_and_sort(results):
+    '''given a query search results, return the average value for the given category
+       and sort the results by descending order
+    
+    Parameters
+    ----------
+    resutls: list of tuples
+        the query search returned by the database
+    
+    Returns
+    -------
+    xvals: list
+        the name of the attribute
+    yvals: list
+        the average value for the corresponding attribute
+    '''
     # result is the list of tuples returned by database query. The tuple must be length of 2!
     dict_rating = {}
     for row in results:
@@ -360,6 +510,26 @@ def get_avg_and_sort(results):
     return xvals, yvals
 
 def flask_plot(xvals, yvals, title, fig_type):
+    ''' this function generetes either bar chart or pie chart 
+        and return the plot that is used for displaying in
+        the html webpage
+
+    Parammeters
+    -----------
+    xvals: list
+        a list of x values
+    yvals: list
+        a list of y values that correspond to the x values
+    title: string
+        the title of the plot
+    fig_type: string
+        either "bar" or "pie" that defines the output plot type
+
+    Returns
+    --------
+    fig_div: string
+        the plot that is readable by html files
+    '''
     fig = make_subplots(rows=1, cols=1, specs=[[{"type": fig_type}]], subplot_titles=(title))
     if fig_type == 'pie':
         fig.add_trace(go.Pie(labels=xvals, values=yvals), row=1, col=1)
@@ -372,6 +542,18 @@ def flask_plot(xvals, yvals, title, fig_type):
     return fig_div
 
 def barplot_city_population():
+    ''' a function that generate a barplot for the population 
+        according to the cities
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     query = '''SELECT Name, Population, State FROM Cities
                 ORDER BY Population DESC'''
     result = searchDB(query)
@@ -387,6 +569,21 @@ def barplot_city_population():
 ########## plots for cities or states ############
 
 def pieplot_restaurant_categories(name, target):
+    ''' a function that generate a pieplot for the percentage
+        of each restaurant category in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Category FROM Restaurants
@@ -421,6 +618,21 @@ def pieplot_restaurant_categories(name, target):
     return flask_plot(labels, values, title, 'pie')
 
 def pieplot_rating(name, target):
+    ''' a function that generate a pieplot for the percentage
+        of ratings of the restaurants in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Rating FROM Restaurants WHERE City="{}"'''.format(name)
@@ -443,6 +655,21 @@ def pieplot_rating(name, target):
     return flask_plot(labels, values, title, 'pie')
 
 def pieplot_price(name, target):
+    ''' a function that generate a pieplot for the percentage
+        of prices of the restaurants in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Price FROM Restaurants WHERE City="{}" AND Price NOTNULL'''.format(name)
@@ -465,6 +692,21 @@ def pieplot_price(name, target):
     return flask_plot(labels, values, title, 'pie')
 
 def barplot_avgrating_each_category(name, target):
+    ''' a function that generate a barplot for the average rating 
+        of differnt restaurant types in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Category, Rating FROM Restaurants WHERE City="{}"'''.format(name)
@@ -478,6 +720,21 @@ def barplot_avgrating_each_category(name, target):
     return flask_plot(xvals, yvals, title, 'bar')
 
 def barplot_avgprice_each_category(name, target):
+    ''' a function that generate a barplot for the average price 
+        of differnt restaurant types in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Category, Price FROM Restaurants
@@ -492,6 +749,21 @@ def barplot_avgprice_each_category(name, target):
     return flask_plot(xvals, yvals, title, 'bar')
   
 def barplot_avgreview_each_category(name, target):
+    ''' a function that generate a barplot for the average number of reviews 
+        of differnt restaurant types in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Category, [Number of Review] FROM Restaurants
@@ -506,6 +778,21 @@ def barplot_avgreview_each_category(name, target):
     return flask_plot(xvals, yvals, title, 'bar')
   
 def barplot_toprated_restaurant(name, target):
+    ''' a function that generate a barplot for the top rated restaurants
+        in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Name, Rating FROM Restaurants WHERE City="{}"
@@ -525,6 +812,21 @@ def barplot_toprated_restaurant(name, target):
     return flask_plot(xvals, yvals, title, 'bar')
    
 def barplot_topprice_restaurant(name, target):
+    ''' a function that generate a barplot for the most 
+        expensive restaurants in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Name, Price FROM Restaurants WHERE City="{}"
@@ -544,6 +846,21 @@ def barplot_topprice_restaurant(name, target):
     return flask_plot(xvals, yvals, title, 'bar')
 
 def barplot_mostreviewed_restaurant(name, target):
+    ''' a function that generate a barplot for the most 
+        reviewed restaurants in the city or state specified 
+
+    Parameters
+    ----------
+    name: string
+        a city name or a state name
+    target: string
+        indicates the name is a city or a state
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     name = process_name(name)
     if target == 'city':
         query = '''SELECT Name, [Number of Review] FROM Restaurants WHERE City="{}"
@@ -565,6 +882,18 @@ def barplot_mostreviewed_restaurant(name, target):
 ########### plots for comparisons ###########
 
 def compare_city_barplot_price():
+    ''' a function that generate a barplot for the average price
+        of all the restaurants in each city. Ranked by descending order.
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     query = '''SELECT City, Price FROM Restaurants WHERE Price NOTNULL'''
     results = searchDB(query)
     xvals, yvals = get_avg_and_sort(results)
@@ -572,6 +901,18 @@ def compare_city_barplot_price():
     return flask_plot(xvals, yvals, title, 'bar')
 
 def compare_city_barplot_rating():
+    ''' a function that generate a barplot for the average rating
+        of all the restaurants in each city. Ranked by descending order.
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     query = '''SELECT City, Rating FROM Restaurants'''
     results = searchDB(query)
     xvals, yvals = get_avg_and_sort(results)
@@ -579,6 +920,18 @@ def compare_city_barplot_rating():
     return flask_plot(xvals, yvals, title, 'bar')
 
 def compare_state_barplot_price():
+    ''' a function that generate a barplot for the average price
+        of all the restaurants in each state. Ranked by descending order.
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     query = '''SELECT c.State, r.Price FROM Cities as c
                JOIN Restaurants as r ON c.Name=r.City
                WHERE Price NOTNULL'''
@@ -588,6 +941,18 @@ def compare_state_barplot_price():
     return flask_plot(xvals, yvals, title, 'bar')
 
 def compare_state_barplot_rating():
+    ''' a function that generate a barplot for the average rating
+        of all the restaurants in each state. Ranked by descending order.
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    string
+        the plot that is readable by html files
+    '''
     query = '''SELECT c.State, r.Rating FROM Cities as c
                JOIN Restaurants as r ON c.Name=r.City'''
     results = searchDB(query)
@@ -601,6 +966,17 @@ def compare_state_barplot_rating():
 
 @app.route('/')
 def home():
+    ''' Function to design the home page
+    
+    Parameters
+    ----------
+    None
+    
+    Returns
+    -------
+    html template that contains city_dict which is a dictionary.
+    The key is the city id, the value is a list that contains city name, state and population
+    '''     
     city_dict = {}
     query = '''SELECT Id, Name, State, Population FROM Cities'''
     results = searchDB(query)
@@ -615,15 +991,49 @@ def home():
 
 @app.route('/population')
 def population():
+    ''' a function a that design the population page which contains a barplot 
+
+    Parameters
+    ----------
+    none
+
+    Returns
+    ----------
+    html template that contains figure which is a plot.
+    '''
     figure = barplot_city_population()
     return render_template('plot.html', figure=Markup(figure))
 
 @app.route('/list/<city_or_state>/<nm>/')
 def choice_list(city_or_state, nm):
+    ''' a function designs the webpage that contains the list of plot options
+
+    Parameters:
+    city_or_state: string
+        either "city" or "state" that indicates if the nm is a city or state
+    nm: string
+        the name of the city or the state
+
+    Returns
+    -------
+    a html template that contains city city_or_state and nm.
+    '''
     return render_template('list.html', city_or_state=city_or_state, name=nm)
 
 @app.route('/plot/<city_or_state>/<nm>/<choice>')
 def data(city_or_state, nm, choice):
+    ''' a function designs the webpage that contains the plot
+
+    Parameters:
+    city_or_state: string
+        either "city" or "state" that indicates if the nm is a city or state
+    nm: string
+        the name of the city or the state
+
+    Returns
+    -------
+    html template that contains figure which is a plot.
+    '''
     name = process_name(nm.replace('%20', ' '))
     if choice == 'pieplot_restaurant_categories':
         figure = pieplot_restaurant_categories(name, city_or_state)
@@ -648,10 +1058,32 @@ def data(city_or_state, nm, choice):
 
 @app.route('/compare/<city_or_state>')
 def compare(city_or_state):
+    ''' a function designs the webpage that contains the plot options for comparison
+
+    Parameters:
+    city_or_state: string
+        either "city" or "state"
+
+    Returns
+    -------
+    html template which contains city_or_state
+    '''
     return render_template('compare.html', city_or_state=city_or_state)
 
 @app.route('/compare/<city_or_state>/<rating_or_price>')
 def compare_choice(city_or_state, rating_or_price):
+    ''' a function designs the webpage that contains the plot for comparison
+
+    Parameters:
+    city_or_state: string
+        either "city" or "state"
+    rating_or_price:
+        either "rating" or "price" that indicates what is being compared
+
+    Returns
+    -------
+    html template which contains the plot
+    '''
     figure = None
     if city_or_state == 'state':
         if rating_or_price == 'rating':
